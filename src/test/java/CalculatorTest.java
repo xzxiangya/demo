2551770b3bbcf27ee7c101b5f2be3dd809f468b5
xuzx <1506453581@qq.com>
2022-04-13 09:20:30 +0800

    add and commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    @Test
+    public void shouldReturn2When1Plus1() {
+        Calculator calculator = new Calculator();
+        int actualResult = calculator.compute(1, 1, '+');
+        int expectResult = 2;
+        Assert.assertEquals(expectResult,actualResult);
+    }
+
+    @Test
+    public void shouldReturn0When1Sub1() {
+        Calculator calculator = new Calculator();
+        int actualResult = calculator.compute(1, 1, '-');
+        int expectResult = 0;
+        Assert.assertEquals(expectResult,actualResult);
+    }
+}

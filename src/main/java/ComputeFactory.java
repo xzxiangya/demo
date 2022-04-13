7816de9eace71c1838666b2b947232c70941530a
xuzx <1506453581@qq.com>
2022-04-13 11:21:43 +0800

    reflection

--- src/main/java/ComputeFactory.java
+++ src/main/java/ComputeFactory.java
@@ -3,14 +3,7 @@ public class ComputeFactory {
     private static final Sub sub = new Sub();
     private static final Add add = new Add();
 
-    public static Computable getComputable(char symbol) {
-        switch (symbol) {
-            case '+':
-                return add;
-            case '-':
-                return sub;
-            default:
-                throw new IllegalArgumentException();
-        }
+    public static Computable getComputable(String symbol) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        return (Computable) Class.forName(symbol).newInstance();
     }
 }
